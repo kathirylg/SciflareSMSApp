a34c4f56bd0b1c779607f4ddc6c8d8aeff7ef165
Kadhir <kathirylg@gmail.com>
2024-03-07 07:32:00 +0530

    Key name changed

--- app/src/main/java/com/sciflare/smsapp/encryption/ConstantValues.kt
+++ app/src/main/java/com/sciflare/smsapp/encryption/ConstantValues.kt
@@ -2,7 +2,7 @@ package com.sciflare.smsapp.encryption
 
 class ConstantValues {
     companion object {
-        val SECRECT_KEY = "25d6c7fe35b9979a161f2136cd13b0ff".toByteArray()
+        val SECRET_KEY = "25d6c7fe35b9979a161f2136cd13b0ff".toByteArray()
         const val IDENTIFICATION_KEY = "sciflare$$"
 
     }
